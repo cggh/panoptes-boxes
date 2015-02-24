37b3d811d0205f5ade45d78ce07b09373d099060
Ian Wright <ian.wright@well.ox.ac.uk>
2015-02-24 14:27:31 +0000

    Networking changes

--- packer/scripts/static-ip.sh
+++ packer/scripts/static-ip.sh
@@ -7,4 +7,4 @@ echo 'auto eth1' >> /etc/network/interfaces
 echo 'iface eth1 inet static' >> /etc/network/interfaces
 echo 'address '$STATIC_IP >> /etc/network/interfaces
 echo 'netmask '$NETMASK >> /etc/network/interfaces
-echo 'gateway '$GW_IP >> /etc/network/interfaces
+#echo 'gateway '$GW_IP >> /etc/network/interfaces
